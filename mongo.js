47f5c9c8b18399968fdf54b60d4522af9b19192f
Eetu Sepp„l„ <eetujuhaniseppala@gmail.com>
2020-11-10 19:51:00 +0200

    Tehtävä 3.12.

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://Admin:${password}@cluster0.wpuyh.mongodb.net/phone_book_app?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(!process.argv[3] || !process.argv[4]) {
+    Person.find({}).then(result => {
+        console.log('Phone book:')
+        result.forEach(person => {
+          console.log(person.name +" "+ person.number)
+        })
+        mongoose.connection.close()
+    })
+}
+else {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(response => {
+        console.log(`Added ${person.name}, number: ${person.number}, to phone book`)
+        mongoose.connection.close()
+    })
+}
