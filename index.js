e087337e66c6940dc6fddc9d2da8f1d5fba7cd0c
Eetu Sepp„l„ <eetujuhaniseppala@gmail.com>
2020-11-18 00:45:32 +0200

    Tehtävät 3.19. - 3.21.

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 
   const body = request.body
 
@@ -84,6 +84,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -106,6 +107,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
 
   next(error)
